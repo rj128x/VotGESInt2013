a7f9d128a47b7e56d11eb0476027463a25e58806
rj128x <rj128x@gmail.com>
2014-09-22 16:45:58 +0600

    chart

---
+++ VotGES/MainSL/Views/OgranPage.xaml.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Animation;
+using System.Windows.Shapes;
+using System.Windows.Navigation;
+
+namespace MainSL.Views
+{
+    public partial class OgranPage : Page
+    {
+        public OgranPage()
+        {
+            InitializeComponent();
+        }
+
+        // Выполняется, когда пользователь переходит на эту страницу.
+        protected override void OnNavigatedTo(NavigationEventArgs e)
+        {
+            List<KeyValuePair<int, int>> lst = new List<KeyValuePair<int, int>>();
+            lst.Add(new KeyValuePair<int, int>(5, 5));
+            serie1.ser
+
+        }
+
+    }
+}
