1b27899a2a417219088dc853f5f5924da4f7c599
rj128x <rj128x@gmail.com>
2014-10-03 15:35:25 +0600

    colors

--- VotGESLib/Data/rem.sql
+++ VotGESLib/Data/rem.sql
@@ -1,11 +1,2 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-
-namespace VotGES.Data
-{
-    class rem
-    {
-    }
-}
+﻿delete from data
+delete from puskstoptable where dateStart>'2014-09-02'
